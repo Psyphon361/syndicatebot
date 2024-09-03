84966314e8e12145a54ebc950bcd7b68a7fe9200
tsharma5 <TSharma5@slb.com>
2024-09-03 12:34:36 +0530

    Fetch latest changes from VPS

--- bot.js
+++ bot.js
@@ -12,15 +12,22 @@ const token = process.env.TELEGRAM_BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
 // Define the group chat IDs and the message to be sent
-const chatIds = process.env.GROUP_CHAT_IDS.split(',');
+const plsChatid = process.env.PLS_GROUP_CHAT_ID;
+const bnbChatid = process.env.BNB_GROUP_CHAT_ID;
+const plsInterval = process.env.PLS_TIME_INTERVAL;
+const bnbInterval = process.env.BNB_TIME_INTERVAL;
+
 const message = process.env.MESSAGE;
 
 // Log when the bot starts
 console.log('Bot has been started...');
 
-// Schedule the message to be sent every 30 seconds
-const job = schedule.scheduleJob(process.env.TIME_INTERVAL, function(){
-  chatIds.forEach(chatId => {
-    bot.sendMessage(chatId, message);
-  });
+// Schedule the message to be sent to the PLS group at the specified interval
+schedule.scheduleJob(plsInterval, function() {
+    bot.sendMessage(plsChatid, message);
+});
+
+// Schedule the message to be sent to the BNB group at the specified interval
+schedule.scheduleJob(bnbInterval, function() {
+    bot.sendMessage(bnbChatid, message);
 });
